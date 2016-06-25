80384b1fee47d402f91b07ada041e7cfdec00c18
Beans <Beans@Beans.local>
2016-06-26 00:47:37 +0800

    创建项目

---
+++ HBSplashScreen_Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBSplashScreen_Objc
+//
+//  Created by Beans on 16/6/26.
+//  Copyright © 2016年 iceWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
